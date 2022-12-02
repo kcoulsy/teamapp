04ae179ec4a366a5d663b9b363e51603809453e7
Kristian Coulson <kristian.coulson@prettylittlething.com>
2022-12-02 16:32:09 +0100

    fix: only use webserver on develop

--- playwright.config.ts
+++ playwright.config.ts
@@ -102,13 +102,15 @@ const config: PlaywrightTestConfig = {
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
   // outputDir: 'test-results/',
+};
 
-  /* Run your local dev server before starting the tests */
-  webServer: {
+/* Run your local dev server before starting the tests */
+if (!process.env.CI) {
+  config.webServer = {
     command: 'npm run dev',
     reuseExistingServer: !process.env.CI,
     url: baseURL,
-  },
-};
+  };
+}
 
 export default config;
